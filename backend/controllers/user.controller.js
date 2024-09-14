ba45b25a155b3591593663910c722ce86f5a4a63
Atishay180 <atishayjain8807@gmail.com>
2024-09-14 18:15:30 +0530

    Refactor server.js to add message and user routes

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,24 @@
+import { User } from "../models/user.model.js";
+
+
+const getUserForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id
+
+        //get all users except the logged in user, "ne" means not equal
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password")
+
+        return res
+            .status(200)
+            .json(filteredUsers)
+
+    } catch (error) {
+        console.log("Error in getUserForSidebar controller: ", error.message);
+        return res
+            .status(500)
+            .json({ error: "Internal Server Error" });
+
+    }
+}
+
+export { getUserForSidebar }
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
 import connectMongoDB from "./db/connectMongoDB.js"
-import authRoutes from "./routes/auth.routes.js"
 
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,6 +21,8 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
