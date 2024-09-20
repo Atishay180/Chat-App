2d5b7d0350cf864379aa18f0735aa94a386dba7c
Atishay180 <atishayjain8807@gmail.com>
2024-09-20 23:10:22 +0530

    Refactor Message component to use dynamic data and add loading spinner

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,39 +1,38 @@
 import React from 'react'
-import {TiMessages} from "react-icons/ti"
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../zustand/useConversation';
+import { extractTime } from '../../utils/extractTime';
 
 
-const Message = () => {
-    const noChatSelected = false;
+const Message = ({ message }) => {
 
+    const { authUser } = useAuthContext();
+    const { selectedConversation } = useConversation();
+
+    //check if this message is sent from the logged in user
+    const fromMe = message.senderId === authUser._id;
+
+    const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+    const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+    
+    const bubbleBgColor = fromMe ? 'bg-blue-500' : '';
+    
+    const formattedTime = extractTime(message.createdAt);
     return (
-        <div className='chat chat-end'>
-            {noChatSelected ? (
-                <NoChatSelected />
-            ) : (
-                <>
-                    <div className='chat-image avatar'>
-                        <div className='w-10 rounded-full'>
-                            <img src="" alt='Tailwind CSS chat bubble component' />
-                        </div>
-                    </div>
-                    <div className={`chat-bubble text-white bg-blue-500`}>Hii! what is upp?</div>
-                </>
-            )}
+        <div className={`chat ${chatClassName}`}>
+            <div className='chat-image avatar'>
+                <div className='w-10 rounded-full'>
+                    <img
+                        src={profilePic}
+                        alt='Tailwind CSS chat bubble component'
+                    />
+                </div>
+            </div>
+            <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+            <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
         </div>
     )
 }
-
-const NoChatSelected = () => {
-    return (
-        <div className='flex items-center justify-center w-full h-full'>
-            <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-                <p>Welcome 👋 Atishay ❄</p>
-                <p>Select a chat to start messaging</p>
-                <TiMessages className='text-3xl md:text-6xl text-center' />
-            </div>
-        </div>
-    );
-};
 
 
 
---
+++ frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import useConversation from "../zustand/useConversation"
+import toast from "react-hot-toast";
+
+
+const useGetMessages = () => {
+    const [loading, setLoading] = useState(false);
+
+    const { messages, setMessages, selectedConversation } = useConversation();
+
+    useEffect(() => {
+        const getMessages = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch(`/api/messages/${selectedConversation._id}`)
+                
+                const data = await res.json();
+
+                if (!res.ok) {
+                    toast.error(data.message || "Something went wrong");
+                    return;
+                }
+
+                setMessages(data);
+            } catch (error) {
+                toast.error(error.message || "Something went wrong");
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        if (selectedConversation?._id) getMessages();
+    }, [selectedConversation?._id, setMessages])
+
+    return { loading, messages };
+}
+
+export default useGetMessages
