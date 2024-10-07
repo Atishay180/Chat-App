e6266cc8331c1b589bd01144c53357e35f71395c
Atishay180 <atishayjain8807@gmail.com>
2024-10-07 23:35:27 +0530

    deployment setup added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv"
 import express from "express"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import connectMongoDB from "./db/connectMongoDB.js"
 
@@ -16,13 +17,28 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("Server is ready")
-})
-
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+
+// --------------------------------------deployment--------------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+    })
+}
+else {
+    app.get("/", (req, res) => {
+        res.send("API is running")
+    })
+}
+
+// --------------------------------------deployment--------------------------------------
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
